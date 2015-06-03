a40ee17f627c13ef4e13b7e7a515deafb2998fab
Shingo Kumagai <shingo4092@gmail.com>
2015-06-03 16:37:41 +0900

    Implement basic content-type for Siren

---
+++ src/Provide/Representation/SirenRenderer.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * This file is part of the BEAR.SirenRenderer package
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+namespace BEAR\SirenRenderer\Provide\Representation;
+
+use Doctrine\Common\Annotations\Reader;
+
+final class SirenRenderer implements RenderInterface
+{
+    /**
+     * @var Reader
+     */
+    private $reader;
+
+    public function __construct(Reader $reader)
+    {
+        $this->reader = $reader;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function render(ResourceObject $ro)
+    {
+        if (!isset($ro->headers['content-type'])) {
+            $ro->headers['content-type'] = 'application/vnd.siren+json';
+        }
+        $ro->view = json_encode($ro);
+        $e = json_last_error();
+        if ($e) {
+            // @codeCoverageIgnoreStart
+            error_log('json_encode error: ' . json_last_error_msg() . ' in ' . __METHOD__);
+            return '';
+            // @codeCoverageIgnoreEnd
+        }
+        return $ro->view;
+    }
+}
