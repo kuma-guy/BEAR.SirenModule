a991c1b366b15d9a2e6c16fa398929831d2bc2f2
shingo-kumagai <shingo4092@gmail.com>
2015-06-05 02:07:34 +0900

    Add url provider

---
+++ src/Provide/UrlProviderInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This file is part of the BEAR.SirenRenderer package
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+namespace BEAR\SirenRenderer\Provide;
+
+interface UrlProviderInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function get();
+}
