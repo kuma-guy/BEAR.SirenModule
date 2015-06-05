b2117c76228ae4edda79a138be84a3289b849e5f
Shingo Kumagai <shingo4092@gmail.com>
2015-06-05 14:52:39 +0900

    Update fake vendor resource for tests

--- tests/Fake/FakeVendor/Sandbox/Resource/App/Order.php
+++ tests/Fake/FakeVendor/Sandbox/Resource/App/Order.php
@@ -4,6 +4,8 @@ namespace FakeVendor\Sandbox\Resource\App;
 
 use BEAR\Resource\ResourceInterface;
 use BEAR\Resource\ResourceObject;
+use BEAR\SirenRenderer\Annotation\Name;
+use BEAR\SirenRenderer\Annotation\Title;
 
 class Order extends ResourceObject
 {
@@ -13,10 +15,42 @@ class Order extends ResourceObject
         $this->resource = $resource;
     }
 
+    /**
+     * @Name("get-item")
+     * @Title("Get Item")
+     *
+     * @param $orderNumber
+     * @return $this
+     */
     public function onGet($orderNumber)
     {
-        $this['itemCount'] = 3;
-        $this['status']    = "pending";
+        $this['orderNumber'] = $orderNumber;
+        $this['itemCount']   = 3;
+        $this['status']      = "pending";
         return $this;
+    }
+
+    /**
+     * @Name("add-item")
+     * @Title("Add Item")
+     */
+    public function onPost()
+    {
+    }
+
+    /**
+     * @Name("delete-item")
+     * @Title("Delete Item")
+     */
+    public function onDelete()
+    {
+    }
+
+    /**
+     * @Name("update-item")
+     * @Title("Update Item")
+     */
+    public function onPut()
+    {
     }
 }
