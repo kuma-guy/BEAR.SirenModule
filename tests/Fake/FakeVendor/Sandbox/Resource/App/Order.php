b7e2b0012278f11c6fc75e63800a433111c51f1d
Shingo Kumagai <shingo4092@gmail.com>
2015-06-08 16:35:28 +0900

    Reformat code & remove unused imports

--- tests/Fake/FakeVendor/Sandbox/Resource/App/Order.php
+++ tests/Fake/FakeVendor/Sandbox/Resource/App/Order.php
@@ -3,18 +3,14 @@
 namespace FakeVendor\Sandbox\Resource\App;
 
 use BEAR\Resource\Annotation\Embed;
-use BEAR\Resource\Annotation\Link;
 use BEAR\Resource\ResourceInterface;
 use BEAR\Resource\ResourceObject;
 use BEAR\SirenRenderer\Annotation\Action;
-use BEAR\SirenRenderer\Annotation\Field;
-use BEAR\SirenRenderer\Annotation\Name;
-use BEAR\SirenRenderer\Annotation\SubEntity;
-use BEAR\SirenRenderer\Annotation\Title;
 
 class Order extends ResourceObject
 {
     private $resource;
+
     public function __construct(ResourceInterface $resource)
     {
         $this->resource = $resource;
@@ -31,8 +27,8 @@ class Order extends ResourceObject
     public function onGet($orderNumber)
     {
         $this['orderNumber'] = $orderNumber;
-        $this['itemCount']   = 3;
-        $this['status']      = "pending";
+        $this['itemCount'] = 3;
+        $this['status'] = "pending";
 
         $customerId = "pj123";
 
