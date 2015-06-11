1420336aa5ccdb83eee0f7c144e05f6f93f77679
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-06-12 02:14:27 +0900

    update file signature

--- src/Annotation/Field.php
+++ src/Annotation/Field.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This file is part of the BEAR.SirenRenderer package
+ * This file is part of the BEAR.SirenModule package
  *
  * @license http://opensource.org/licenses/MIT MIT
  */
